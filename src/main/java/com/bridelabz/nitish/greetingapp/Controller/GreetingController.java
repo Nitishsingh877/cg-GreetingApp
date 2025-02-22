0beac38a795bc7e68161e88e06bb57d8d14ca62c
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-22 16:06:30 +0530

    [Nitish] added code for list all messages

--- src/main/java/com/bridelabz/nitish/greetingapp/Controller/GreetingController.java
+++ src/main/java/com/bridelabz/nitish/greetingapp/Controller/GreetingController.java
@@ -5,25 +5,28 @@ import com.bridelabz.nitish.greetingapp.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class GreetingController {
     @Autowired
     private GreetingRepository greetingServices;
-
+    //uc01
     @PostMapping("/greeting")
     public Greeting saveGreeting(@RequestBody Greeting greeting) {
         return greetingServices.save(greeting);
     }
-//    @GetMapping("/get/greetings")
-//    public List<Greeting> getGreetings() {
-//        return greetingServices.findAll();
-//    }
+    //uc02
     @GetMapping("/get/{id}")
     public Greeting getGreeting(@PathVariable long id) {
         Optional<Greeting> greeting = greetingServices.findById(id);
         return greeting.orElse(null);
+    }
+    //uc03
+    @GetMapping("/get/greetings")
+    public List<Greeting> getGreetings() {
+        return greetingServices.findAll();
     }
 
 }
